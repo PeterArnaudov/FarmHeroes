99951e39a283f09c78e23695fd5e8585568f4987
PeterArnaudov <pepitobg99@gmail.com>
2020-02-22 02:30:16 +0200

    logged in users are redirected straight to  /hero/overview

--- FarmHeroes/Web/FarmHeroes.Web/Controllers/HomeController.cs
+++ FarmHeroes/Web/FarmHeroes.Web/Controllers/HomeController.cs
@@ -10,6 +10,11 @@
     {
         public IActionResult Index()
         {
+            if (this.User.Identity.IsAuthenticated)
+            {
+                return this.Redirect("/Hero/Overview");
+            }
+
             return this.View();
         }
 
