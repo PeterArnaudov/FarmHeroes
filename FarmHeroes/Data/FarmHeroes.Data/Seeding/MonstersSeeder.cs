45be14ceddea6ea0effdb9c9d28eac59e44e2ada
PeterArnaudov <pepitobg99@gmail.com>
2020-03-21 21:27:37 +0200

    optimization: monster images are internal

--- FarmHeroes/Data/FarmHeroes.Data/Seeding/MonstersSeeder.cs
+++ FarmHeroes/Data/FarmHeroes.Data/Seeding/MonstersSeeder.cs
@@ -16,15 +16,15 @@
 
             Monster[] monsters = new Monster[]
             {
-                new Monster() { Name = "Zombie Miner", AvatarUrl = "https://i.ibb.co/4pV11MX/zombie-miner.jpg", Level = 1, StatPercentage = 50 },
-                new Monster() { Name = "Scarecrow", AvatarUrl = "https://i.ibb.co/JrXttjC/scarecrow.jpg", Level = 2, StatPercentage = 55},
-                new Monster() { Name = "Fiery Chicken", AvatarUrl = "https://i.ibb.co/WDkLjDq/fiery-chicken.jpg", Level = 3, StatPercentage = 70 },
-                new Monster() { Name = "Green Slime", AvatarUrl = "https://i.ibb.co/yyr7X5D/green-slime.jpg", Level = 4, StatPercentage = 80},
-                new Monster() { Name = "Hypnotizing Cat", AvatarUrl = "https://i.ibb.co/7WXq09G/hypnotizing-cat.jpg", Level = 5, StatPercentage = 90},
-                new Monster() { Name = "Madshroom", AvatarUrl = "https://i.ibb.co/Rpnvxnd/madshroom.jpg", Level = 6, StatPercentage = 100},
-                new Monster() { Name = "Panda-Assassin", AvatarUrl = "https://i.ibb.co/kmgkFpM/panda-assassin.jpg", Level = 7, StatPercentage = 110 },
-                new Monster() { Name = "Squeen", AvatarUrl = "https://i.ibb.co/1J8nbVF/squeen.jpg", Level = 8, StatPercentage = 125},
-                new Monster() { Name = "Zirkus Firus", AvatarUrl = "https://i.ibb.co/DbJ9J10/zirkus-firus.jpg", Level = 9, StatPercentage = 150 },
+                new Monster() { Name = "Zombie Miner", AvatarUrl = "/images/monsters/zombie-miner.jpg", Level = 1, StatPercentage = 50 },
+                new Monster() { Name = "Scarecrow", AvatarUrl = "/images/monsters/scarecrow.jpg", Level = 2, StatPercentage = 55},
+                new Monster() { Name = "Fiery Chicken", AvatarUrl = "/images/monsters/fiery-chicken.jpg", Level = 3, StatPercentage = 70 },
+                new Monster() { Name = "Green Slime", AvatarUrl = "/images/monsters/green-slime.jpg", Level = 4, StatPercentage = 80},
+                new Monster() { Name = "Hypnotizing Cat", AvatarUrl = "/images/monsters/hypnotizing-cat.jpg", Level = 5, StatPercentage = 90},
+                new Monster() { Name = "Madshroom", AvatarUrl = "/images/monsters/madshroom.jpg", Level = 6, StatPercentage = 100},
+                new Monster() { Name = "Panda-Assassin", AvatarUrl = "/images/monsters/panda-assassin.jpg", Level = 7, StatPercentage = 110 },
+                new Monster() { Name = "Squeen", AvatarUrl = "/images/monsters/squeen.jpg", Level = 8, StatPercentage = 125},
+                new Monster() { Name = "Zirkus Firus", AvatarUrl = "/images/monsters/zirkus-firus.jpg", Level = 9, StatPercentage = 150 },
             };
 
             await dbContext.Monsters.AddRangeAsync(monsters);
