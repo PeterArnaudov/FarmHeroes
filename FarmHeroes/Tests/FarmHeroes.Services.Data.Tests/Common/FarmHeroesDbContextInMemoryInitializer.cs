c39cf71aed7d266c7307ab833710a4361d3d542f
PeterArnaudov <pepitobg99@gmail.com>
2020-04-02 02:16:54 +0300

    added common unit testing code

---
+++ FarmHeroes/Tests/FarmHeroes.Services.Data.Tests/Common/FarmHeroesDbContextInMemoryInitializer.cs
@@ -0,0 +1,19 @@
+﻿namespace FarmHeroes.Services.Data.Tests.Common
+{
+    using System;
+
+    using FarmHeroes.Data;
+    using Microsoft.EntityFrameworkCore;
+
+    public class FarmHeroesDbContextInMemoryInitializer
+    {
+        public static FarmHeroesDbContext InitializeContext()
+        {
+            var options = new DbContextOptionsBuilder<FarmHeroesDbContext>()
+               .UseInMemoryDatabase(databaseName: "farm_heroes_db")
+               .Options;
+
+            return new FarmHeroesDbContext(options);
+        }
+    }
+}
---
+++ FarmHeroes/Tests/FarmHeroes.Services.Data.Tests/Common/MapperInitializer.cs
@@ -0,0 +1,19 @@
+﻿namespace FarmHeroes.Services.Data.Tests.Common
+{
+    using System.Reflection;
+
+    using FarmHeroes.Data.Models.HeroModels;
+    using FarmHeroes.Services.Mapping;
+    using FarmHeroes.Services.Models.Monsters;
+    using FarmHeroes.Web.ViewModels.HeroModels;
+
+    public class MapperInitializer
+    {
+        public static void InitializeMapper()
+        {
+            AutoMapperConfig.RegisterMappings(
+                typeof(HeroCreateInputModel).GetTypeInfo().Assembly,
+                typeof(Hero).GetTypeInfo().Assembly);
+        }
+    }
+}
