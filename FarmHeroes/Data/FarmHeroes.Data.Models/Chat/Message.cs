b0cf50d5054f0d6f1cea926c7224a6314d5237d9
PeterArnaudov <pepitobg99@gmail.com>
2020-04-09 22:53:55 +0300

    added missing files

---
+++ FarmHeroes/Data/FarmHeroes.Data.Models/Chat/Message.cs
@@ -0,0 +1,15 @@
+﻿namespace FarmHeroes.Data.Models.Chat
+{
+    using System;
+
+    public class Message
+    {
+        public int Id { get; set; }
+
+        public string Sender { get; set; }
+
+        public string Text { get; set; }
+
+        public string CreatedOn { get; set; }
+    }
+}
---
+++ FarmHeroes/Data/FarmHeroes.Data/Configurations/ChatConfigurations/MessageConfiguration.cs
@@ -0,0 +1,16 @@
+﻿namespace FarmHeroes.Data.Configurations.ChatConfigurations
+{
+    using FarmHeroes.Data.Models.Chat;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public class MessageConfiguration : IEntityTypeConfiguration<Message>
+    {
+        public void Configure(EntityTypeBuilder<Message> builder)
+        {
+            builder.HasKey(m => m.Id);
+
+            builder.ToTable("Messages");
+        }
+    }
+}
