676864b3e8c2f59e83c3a0e2994a5108db659fe4
PeterArnaudov <pepitobg99@gmail.com>
2020-06-11 15:55:08 +0300

    added documentation to FarmHeroesException

--- FarmHeroes/Services/FarmHeroes.Services.Data/Exceptions/FarmHeroesException.cs
+++ FarmHeroes/Services/FarmHeroes.Services.Data/Exceptions/FarmHeroesException.cs
@@ -2,8 +2,23 @@
 {
     using System;
 
+    /// <summary>
+    /// Custom exception tailored to fulfill the needs of the application.
+    /// </summary>
     public class FarmHeroesException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FarmHeroesException"/> class with a specified message, instructions and redirect path.
+        /// </summary>
+        /// <param name="message">
+        /// The message that describes what's gone wrong.
+        /// </param>
+        /// <param name="instructions">
+        /// Instructions to help the user.
+        /// </param>
+        /// <param name="redirectPath">
+        /// A path where to redirect the user upon thrown <see cref="FarmHeroesException"/>.
+        /// </param>
         public FarmHeroesException(string message, string instructions, string redirectPath)
             : base(message)
         {
@@ -11,8 +26,14 @@
             this.RedirectPath = redirectPath;
         }
 
+        /// <summary>
+        /// Gets or sets a message that instructs the user.
+        /// </summary>
         public string Instructions { get; set; }
 
+        /// <summary>
+        /// Gets or sets a redirect path used by <see cref="FarmHeroes.Web.Filters.FarmHeroesExceptionFilterAttribute"/> to redirect the user.
+        /// </summary>
         public string RedirectPath { get; set; }
     }
 }
