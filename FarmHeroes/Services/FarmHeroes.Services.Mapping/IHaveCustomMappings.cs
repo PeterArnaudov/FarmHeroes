5cfe88722e6fcfadbbc9a0469c606b1ba4f2e768
PeterArnaudov <pepitobg99@gmail.com>
2020-07-02 14:33:26 +0300

    removed unused from the template

--- FarmHeroes/Services/FarmHeroes.Services.Mapping/IHaveCustomMappings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FarmHeroes.Services.Mapping
-{
-    using AutoMapper;
-
-    public interface IHaveCustomMappings
-    {
-        void CreateMappings(IProfileExpression configuration);
-    }
-}
--- FarmHeroes/Services/FarmHeroes.Services.Mapping/IMapFrom.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FarmHeroes.Services.Mapping
-{
-    // ReSharper disable once UnusedTypeParameter
-    public interface IMapFrom<T>
-    {
-    }
-}
