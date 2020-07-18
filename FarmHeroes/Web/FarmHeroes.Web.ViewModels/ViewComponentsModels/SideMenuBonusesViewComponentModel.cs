26ce2e005f6efed4d16420e92b69a9d11294ee08
PeterArnaudov <pepitobg99@gmail.com>
2020-07-19 02:03:14 +0300

    fixed visual bug

--- FarmHeroes/Web/FarmHeroes.Web.ViewModels/ViewComponentsModels/SideMenuBonusesViewComponentModel.cs
+++ FarmHeroes/Web/FarmHeroes.Web.ViewModels/ViewComponentsModels/SideMenuBonusesViewComponentModel.cs
@@ -1,9 +1,15 @@
 ﻿namespace FarmHeroes.Web.ViewModels.ViewComponentsModels
 {
+    using System;
+    using System.Linq;
     using FarmHeroes.Web.ViewModels.EquipmentModels;
 
     public class SideMenuBonusesViewComponentModel
     {
         public BonusViewModel[] Bonuses { get; set; }
+
+        public BonusViewModel[] ActiveBonuses => this.Bonuses
+            .Where(x => x.ActiveUntil != DateTime.MaxValue && x.ActiveUntil > DateTime.UtcNow)
+            .ToArray();
     }
 }
