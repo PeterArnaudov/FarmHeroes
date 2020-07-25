aead4a077b63afac5ad54a9eb6b9b07de4aa2c42
PeterArnaudov <pepitobg99@gmail.com>
2020-07-26 00:51:13 +0300

    removed logic from inventory view

--- FarmHeroes/Web/FarmHeroes.Web.ViewModels/InventoryModels/InventoryViewModel.cs
+++ FarmHeroes/Web/FarmHeroes.Web.ViewModels/InventoryModels/InventoryViewModel.cs
@@ -3,6 +3,7 @@
     using FarmHeroes.Data.Models.HeroModels;
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     public class InventoryViewModel
     {
@@ -10,10 +11,20 @@
 
         public int UpgradeCost { get; set; }
 
+        public bool MaximumCapacityReached => this.MaximumCapacity >= 20;
+
         public List<HeroEquipment> Items { get; set; }
+
+        public bool HasItems => this.Items.Count > 0;
 
         public List<HeroAmulet> Amulets { get; set; }
 
+        public bool HasAmulets => this.Amulets.Count > 0;
+
         public List<HeroBonus> Bonuses { get; set; }
+
+        public List<HeroBonus> ActiveBonuses => this.Bonuses.Where(x => x.ActiveUntil > DateTime.UtcNow).ToList();
+
+        public bool HasBonuses => this.Bonuses.Count > 0;
     }
 }
