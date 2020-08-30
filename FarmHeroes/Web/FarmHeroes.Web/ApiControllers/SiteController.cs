38617f338aaad7638ddbc2b994cf4ac6c856c759
PeterArnaudov <pepitobg99@gmail.com>
2020-08-30 20:30:15 +0300

    added missing api controller to the commit that contains localization introduction

---
+++ FarmHeroes/Web/FarmHeroes.Web/ApiControllers/SiteController.cs
@@ -0,0 +1,36 @@
+﻿namespace FarmHeroes.Web.ApiControllers
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Localization;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class SiteController : ApiController
+    {
+        [HttpGet("SetLanguage/{culture}")]
+        public ActionResult SetLanguage(string culture)
+        {
+            string[] validCultures = new[] { "en", "bg" };
+
+            if (!validCultures.Contains(culture))
+            {
+                return this.BadRequest();
+            }
+
+            this.Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions
+                {
+                    Expires = DateTime.UtcNow.AddYears(1),
+                    IsEssential = true,  // critical settings to apply new culture
+                    Path = "/",
+                    HttpOnly = false,
+                });
+
+            return this.Ok();
+        }
+    }
+}
