5c2666cde46b883f9bf15a6b90fee5c3129985b7
PeterArnaudov <pepitobg99@gmail.com>
2020-09-16 00:23:04 +0300

    refactored farm ajax; added farm localization

--- FarmHeroes/Web/FarmHeroes.Web/Areas/Village/Controllers/FarmController.cs
+++ FarmHeroes/Web/FarmHeroes.Web/Areas/Village/Controllers/FarmController.cs
@@ -6,20 +6,28 @@
     using FarmHeroes.Data.Models.Enums;
     using FarmHeroes.Data.Models.HeroModels;
     using FarmHeroes.Services.Data.Contracts;
+    using FarmHeroes.Services.Data.Formulas;
     using FarmHeroes.Web.Filters;
     using FarmHeroes.Web.ViewModels.FarmModels;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Localization;
 
     [Authorize]
     [Area("Village")]
     public class FarmController : BaseController
     {
         private readonly IHeroService heroService;
+        private readonly IFarmService farmService;
+        private readonly IChronometerService chronometerService;
+        private readonly IStringLocalizer<FarmController> stringLocalizer;
 
-        public FarmController(IHeroService heroService)
+        public FarmController(IHeroService heroService, IFarmService farmService, IChronometerService chronometerService, IStringLocalizer<FarmController> stringLocalizer)
         {
             this.heroService = heroService;
+            this.farmService = farmService;
+            this.chronometerService = chronometerService;
+            this.stringLocalizer = stringLocalizer;
         }
 
         public async Task<IActionResult> Index()
@@ -29,6 +37,39 @@
             FarmViewModel viewModel = await this.heroService.GetCurrentHeroViewModel<FarmViewModel>();
 
             return this.View(viewModel);
+        }
+
+        public async Task<IActionResult> StartWork()
+        {
+            int seconds = await this.farmService.StartWork();
+
+            return this.Json(new
+            {
+                seconds,
+                timeLeftParagraph = this.stringLocalizer["Work-Time-Left-Paragraph"].Value,
+                cancelWorkParagraph = this.stringLocalizer["Cancel-Work-Paragraph"].Value,
+                cancelButton = this.stringLocalizer["Cancel-Button-Text"].Value,
+            });
+        }
+
+        public async Task<IActionResult> Collect()
+        {
+            return this.Json(await this.farmService.Collect());
+        }
+
+        public async Task<IActionResult> CancelWork()
+        {
+            await this.chronometerService.NullifyWorkUntil();
+
+            int salary = FarmFormulas.CalculateFarmSalaryPerHour(this.heroService.GetHero().Result.Level.CurrentLevel);
+
+            return this.Json(new
+            {
+                salary,
+                salaryParagraph = this.stringLocalizer["Current-Salary-Paragraph"].Value,
+                durationParagraph = this.stringLocalizer["Work-Duration-Paragraph"].Value,
+                workButton = this.stringLocalizer["Work-Button-Text"].Value,
+            });
         }
     }
 }
--- FarmHeroes/Web/FarmHeroes.Web/wwwroot/js/site.js
+++ FarmHeroes/Web/FarmHeroes.Web/wwwroot/js/site.js
@@ -70,6 +70,14 @@ function numberWithSeparator(target, separator) {
     return target.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
+function updateGold(gold) {
+    $('#current-gold').text(numberWithSeparator(Number($('#current-gold').text().replace(/[\s\.,]/g, "")) + Number(gold)));
+}
+
+function updateCrystals(crystals) {
+    $('#current-crystals').text(numberWithSeparator(Number($('#current-crystals').text().replace(/[\s\.,]/g, "")) + Number(crystals)));
+}
+
 function showAlert(error) {
     let $alert = $(`<div class='alert alert-danger alert-dismissible fade show' role='alert'><h4 class='alert-heading font-weight-bolder'>Oups! Something went wrong.</h4><p class='mb-0'><span class='font-weight-bold'>Cause: </span>${error.responseJSON.message}</p > <p><span class='font-weight-bold'>Instructions: </span>${error.responseJSON.instructions}</p><hr><p class='mb-0 text-muted font-italic text-right'>You might have gone against the rules and tried to do something not allowed.</p><button type='button' class='close' data-dismiss='alert'><span aria-hidden='true'>&times;</span></button></div>`);
     $("#view-body").prepend($alert);
