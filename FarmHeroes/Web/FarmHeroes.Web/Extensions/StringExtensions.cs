172f846f271cf2d5f71ddae0343d0bf74b6b4c56
PeterArnaudov <pepitobg99@gmail.com>
2020-10-18 18:34:48 +0300

    front end changes

---
+++ FarmHeroes/Web/FarmHeroes.Web/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace FarmHeroes.Web.Extensions
+{
+    using System.Text.RegularExpressions;
+
+    public static class StringExtensions
+    {
+        public static string ToFriendlyCase(this string pascalString)
+        {
+            return Regex.Replace(pascalString, "(?!^)([A-Z])", " $1");
+        }
+    }
+}
