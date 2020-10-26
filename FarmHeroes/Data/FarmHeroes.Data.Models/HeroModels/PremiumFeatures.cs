fadb8becc29ad4d7cecc3d20f132aa50339f42fd
PeterArnaudov <pepitobg99@gmail.com>
2020-10-26 22:32:53 +0200

    added missing files

---
+++ FarmHeroes/Data/FarmHeroes.Data.Models/HeroModels/PremiumFeatures.cs
@@ -0,0 +1,11 @@
+﻿namespace FarmHeroes.Data.Models.HeroModels
+{
+    public class PremiumFeatures
+    {
+        public int Id { get; set; }
+
+        public bool HarbourManager { get; set; }
+
+        public virtual Hero Hero { get; set; }
+    }
+}
---
+++ FarmHeroes/Data/FarmHeroes.Data/Configurations/HeroConfigurations/PremiumFeaturesConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace FarmHeroes.Data.Configurations.HeroConfigurations
+{
+    using FarmHeroes.Data.Models.HeroModels;
+
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public class PremiumFeaturesConfiguration : IEntityTypeConfiguration<PremiumFeatures>
+    {
+        public void Configure(EntityTypeBuilder<PremiumFeatures> builder)
+        {
+            builder.ToTable("PremiumFeatures");
+
+            builder.HasKey(pf => pf.Id);
+
+            builder.HasOne(pf => pf.Hero)
+                .WithOne(h => h.PremiumFeatures)
+                .HasForeignKey<Hero>(h => h.PremiumFeaturesId);
+        }
+    }
+}
